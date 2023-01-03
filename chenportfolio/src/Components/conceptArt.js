d71e375238fd295c9af270b0e2f97ca78d9df195
EngineNeo <neomaralit@gmail.com>
2023-01-03 04:37:58 -0500

    began integrating photoswipe

--- chenportfolio/src/Components/conceptArt.js
+++ chenportfolio/src/Components/conceptArt.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 import { ProGallery } from 'pro-gallery';
 import { Link } from 'react-router-dom';
+import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
+
+const psoptions = {
+  dataSource: [
+
+    // simple image
+    {
+      src: 'https://source.unsplash.com/Volo9FYUAzU/1620x1080',
+      width: 1620,
+      height: 1080,
+      alt: 'test image 1'
+    },
+
+    {
+      src: 'https://source.unsplash.com/RJzHlbKf6eY/1950x1300',
+      width: 1950,
+      height: 1300,
+      alt: 'test image 2'
+    },
+
+    // responsive image
+    {
+      srcset: 'https://dummyimage.com/1500x1000/555/fff/?text=1500x1000 1500w, https://dummyimage.com/1200x800/555/fff/?text=1200x800 1200w, https://dummyimage.com/600x400/555/fff/?text=600x400 600w',
+      src: 'https://dummyimage.com/1500x1000/555/fff/?text=1500x1000',
+      width: 1500,
+      height: 1000,
+      alt: 'test image 3',
+    },
+
+    // HTML slide
+    {
+      html: '<div class="custom-html-slide">This is custom HTML slide. <a href="http://example.com" target="_blank" rel="nofollow">Test Link</a>.</div>'
+    }
+
+  ],
+  showHideAnimationType: 'none',
+  pswpModule: () => import('./photoswipe.esm.js'),
+};
+const lightbox = new PhotoSwipeLightbox(psoptions);
+lightbox.init();
 
 // The options of the gallery
 const options = {
   galleryLayout: -1,
   gallerySize: 50,
   imageHoverAnimation: 'ZOOM_IN',
+  itemmClick: 'expand'
 };
 
 // The size of the gallery container. The images will fit themselves in it
@@ -46,14 +87,31 @@ class ConceptArt extends Component {
 
     const customInfoRenderer = (itemProps) =>
       (
-        <div class="hover-info-title">
+        <div className="hover-info-title">
           <h5>{itemProps.title}</h5>
         </div>
       );
 
+      const customImageRenderer = (imageProps) =>
+      (
+        <div>
+          <img
+          alt={imageProps.alt}
+          style={{
+            width: imageProps.style.width,
+            height: imageProps.style.height
+          }}
+          src={imageProps.src}
+          />
+        </div >
+      );
+
     const eventsListener = (eventName, eventData) =>
-    console.log({ eventName, eventData });
-  
+    console.log({eventName, eventData});
+
+    // const imageContainer = document.getElementsByClassName("gallery-item-hover-inner")
+    // imageContainer[0].onclick = function(){lightbox.loadAndOpen(0)};
+      
     return (
         <section id="conceptArt">
 
@@ -65,41 +123,15 @@ class ConceptArt extends Component {
             container={container}
             eventsListener={eventsListener}
             customHoverRenderer={customInfoRenderer}
+            customImageRenderer={customImageRenderer}
           />
-          
+
           <Link to="/moreCA">
               <button className="cool-button">See More</button>
           </Link>
-          
-            {/* <Gallery
 
-            rowHeight={100}
-            enableLightbox={false}
-            enableImageSelection={false}
-            >
-              {Object.values(this.state.studies).map((post, divKey) => {
-                      return (
-                        <div key={divKey}>
-                          <Item
-                            original={post.original}
-                            thumbnail={post.thumbnail}
-                            width={post.width}
-                            height={post.height}
-                          >
-                            {({ ref, open }) => (
-                              <img className="imageItem" 
-                              ref={ref} 
-                              onClick={open}
-                              src={post.thumbnail}
-                              alt="Concept Art" />
-                            )}
-                          </Item>
-                        </div>
-                      );
-                    })}
-            </Gallery> */}
         </section>
-        
+
     );
   }
 }
